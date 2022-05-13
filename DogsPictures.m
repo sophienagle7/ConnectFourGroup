5784e4a2a09ce361e0013fa2c9da14aa0b2de816
ashleygiannetti <ashley_giannetti@brown.edu>
2022-05-12 22:04:27 -0400

    commenting code on homepage and dog pics- likely to transfer information into README

--- DogsPictures.m
+++ DogsPictures.m
@@ -1,2 +1,15 @@
 I = imread('HomePage.png');
 imshow(I)
+
+%this is a simple script that displays an image of dogs
+%this script is run when the third choice button is selected on the home
+%screen
+%I wanted to add this sipt because one of the valuabel things I learend in
+%this proiject was how to import an image inot a script and display it. I
+%needed this skill because I was originally planning on importing an image
+%as the menu and using gipout as the selection option to choose each script
+%This did not work out but I still wanted to demonstrate the image skill
+%and add a little more fun to the final product, so I added this image of
+%dogs to the menu options. 
+
+%driver: Ashley
--- homepage.m
+++ homepage.m
@@ -6,6 +6,9 @@ set(0, 'DefaultUIControlFontSize', 18);
 choice = menu(msg,opts);
 set(0, 'DefaultUIControlFontSize', UIControl_FontSize_bak);
 
+%menu function in matlab creating a homescreen that allows the player to
+%select which option they want to pursue bu clicking on the appropriate box
+
 if choice == 1
     ConnectFour_Final()
 elseif choice == 2
@@ -15,7 +18,19 @@ elseif choice == 3
 
 
 end
+ 
+% we then used an if statement that works in tandem with the menu function.
+% We identified each of the three options as choices 1, 2, and 3. We then
+% incorporated this knowledge into the if statement to direct the menu
+% function to other scropts within the folder so that when the player
+% selects the option they want, they are taken to the appropriate script. 
+
+%We originally intended to change the colors of the menu page to
+%personalize it, but after extensice research and attempts we found that
+%this was not possible with the menu fucntion in matlab. Thus, to spice up
+%our project in another way, we added another option to our script that
+%enables the user to look at a picture of cute dogs when clicked on
 
 
 
-
+%driver: Ashley 
